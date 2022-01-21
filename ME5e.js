19a772935909223dbc3bba2b5e5a504f6c694fae
lancegonzalez7 <lance.gonzalez7@gmail.com>
2022-01-21 01:15:09 -0700

    Update ME5e.js

--- ME5e.js
+++ ME5e.js
@@ -4,6 +4,6 @@ Hooks.once("init", function() {
     console.log("ME5e | Initializing ME5e System");
 
     Items.unregisterSheet("core", ItemSheet);
-    Items.registerSheet("ME5e", ME5eItemSheet, {makeDefault: true});
+    Items.registerSheet("Foundry_ME5e", ME5eItemSheet, {makeDefault: true});
 
 });
