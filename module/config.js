0c2187384b4c390cec7badfa6d178eaf08d131dc
lancegonzalez7 <lance.gonzalez7@gmail.com>
2022-01-21 02:52:50 -0700

    Update config.js

--- module/config.js
+++ module/config.js
@@ -2,18 +2,11 @@ export const ME5e = {};
 
 ME5e.weaponType = {
     none: "",
-    assaultRifle: ME5e.weaponTypeList.assaultRifle,
-    heavyPistol: ME5e.weaponTypeList.heavyPistol,
-    heavyWeapon: ME5e.weaponTypeList.heavyWeapon,
-    melee: ME5e.weaponTypeList.melee,
-    shotgun: ME5e.weaponTypeList.shotgun,
-    smg: ME5e.weaponTypeList.smg,
-    sniperRifle: ME5e.weaponTypeList.sniperRifle
-    /*assaultRifle: "Assault Rifle",
+    assaultRifle: "Assault Rifle",
     heavyPistol: "Heavy Pistol",
     heavyWeapon: "Heavy Weapon",
     melee: "Melee",
     shotgun: "Shotgun",
     smg: "SMG",
-    sniperRifle: "Sniper Rifle"*/
+    sniperRifle: "Sniper Rifle"
 }
