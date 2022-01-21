e4353b60b970c07038af1ad476846559dc58daa2
lancegonzalez7 <lance.gonzalez7@gmail.com>
2022-01-21 01:08:48 -0700

    Update ME5e.js

--- templates/sheets/ME5e.js
+++ templates/sheets/ME5e.js
@@ -3,7 +3,7 @@ import ME5eItemSheet from "../../modules/sheets/ME5eItemSheet.js";
 Hooks.once("init", function() {
     console.log("ME5e | Initializing ME5e System");
 
-    ME5eItemSheet.unregisterSheet("core", ItemSheet);
-    ME5eItemSheet.registerSheet("Foundry_ME5e", ME5eItemSheet, {makeDefault: true});
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("Foundry_ME5e", ME5eItemSheet, {makeDefault: true});
 
 });
